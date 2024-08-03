07ca03fa3b2e08b523cd7a925f1799c0bcce6b57
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-04 00:51:35 +0200

    Add spicetify color variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spice: {
+          text: "var(--spice-text)",
+          subtext: "var(--spice-subtext)",
+          main: {
+            DEFAULT: "var(--spice-main)",
+            elevated: "var(--spice-main-elevated)",
+          },
+          highlight: {
+            DEFAULT: "var(--spice-highlight)",
+            elevated: "var(--spice-highlight-elevated)",
+          },
+          sidebar: "var(--spice-sidebar)",
+          player: "var(--spice-player)",
+          card: "var(--spice-card)",
+          shadow: "var(--spice-shadow)",
+          "selected-row": "var(--spice-selected-row)",
+          button: {
+            DEFAULT: "var(--spice-button)",
+            active: "var(--spice-button-active)",
+            disabled: "var(--spice-button-disabled)",
+          },
+          "tab-active": "var(--spice-tab-active)",
+          notification: {
+            DEFAULT: "var(--spice-notification)",
+            error: "var(--spice-notification-error)",
+          },
+          misc: "var(--spice-misc)",
+        },
+      },
+    },
   },
   plugins: [],
 };
