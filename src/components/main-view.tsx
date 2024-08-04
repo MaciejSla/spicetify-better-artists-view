9435f2a692837a0c079c84d7eba2956906485548
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-08-04 18:37:05 +0200

    Move constants to seperate file

--- src/components/main-view.tsx
+++ src/components/main-view.tsx
@@ -6,29 +6,7 @@ import { getCurrentArtist, getAlbumsByArtist } from "../utils/fetchHelpers";
 import { cn } from "../utils/general";
 import { SearchIcon } from "./icons";
 import fuzzysort from "fuzzysort";
-
-const LOCAL_STORAGE_PREFIX = "better-artists";
-const ALBUM_FETCH_URL = "https://api.spotify.com/v1/me/albums?limit=50";
-const COLORS = [
-  "bg-spice-text",
-  "bg-spice-subtext",
-  "bg-spice-main",
-  "bg-spice-main-elevated",
-  "bg-spice-highlight",
-  "bg-spice-highlight-elevated",
-  "bg-spice-sidebar",
-  "bg-spice-player",
-  "bg-spice-card",
-  "bg-spice-shadow",
-  "bg-spice-selected-row",
-  "bg-spice-button",
-  "bg-spice-button-active",
-  "bg-spice-button-disabled",
-  "bg-spice-tab-active",
-  "bg-spice-notification",
-  "bg-spice-notification-error",
-  "bg-spice-misc",
-];
+import { LOCAL_STORAGE_PREFIX, ALBUM_FETCH_URL, COLORS } from "../constants";
 
 export default function MainView() {
   const el = document.querySelector<HTMLElement>(".Root__main-view");
---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const LOCAL_STORAGE_PREFIX = "better-artists";
+export const ALBUM_FETCH_URL = "https://api.spotify.com/v1/me/albums?limit=50";
+export const COLORS = [
+  "bg-spice-text",
+  "bg-spice-subtext",
+  "bg-spice-main",
+  "bg-spice-main-elevated",
+  "bg-spice-highlight",
+  "bg-spice-highlight-elevated",
+  "bg-spice-sidebar",
+  "bg-spice-player",
+  "bg-spice-card",
+  "bg-spice-shadow",
+  "bg-spice-selected-row",
+  "bg-spice-button",
+  "bg-spice-button-active",
+  "bg-spice-button-disabled",
+  "bg-spice-tab-active",
+  "bg-spice-notification",
+  "bg-spice-notification-error",
+  "bg-spice-misc",
+];
