13a4942d19ab794b9ce3f5f65f052d6fac799681
MaciejSla <m.sladkiewicz.633@studms.ug.edu.pl>
2024-10-12 20:09:12 +0200

    Bring custom album card style closer to the premade one

--- src/components/custom-album-card.tsx
+++ src/components/custom-album-card.tsx
@@ -6,32 +6,50 @@ export function CustomAlbumCard({ album }: { album: Album }) {
     <a
       href={album.uri}
       key={album.id}
-      className="group relative flex h-full flex-col gap-1 rounded-md p-4 hover:no-underline"
+      className="group relative flex h-full flex-col gap-2 rounded-md p-3 !no-underline"
     >
-      <img
-        src={album.images ? album.images[1]?.url : ""}
-        alt={album.name}
-        className="z-30 aspect-square w-full rounded-md shadow-lg"
-      />
+      <div
+        className="relative z-30 aspect-square overflow-hidden rounded-md"
+        style={{ boxShadow: "0 8px 24px rgba(var(--spice-rgb-shadow),.5)" }}
+      >
+        <img
+          src={album.images ? album.images[1]?.url : ""}
+          alt={album.name}
+          className="aspect-square w-full object-cover object-center"
+        />
+      </div>
       <div className="z-30 flex flex-col gap-1">
         <div
           data-tippy-content={album.name}
-          className="line-clamp-1 text-base text-spice-text hover:underline"
+          className="line-clamp-2 text-base text-spice-text hover:underline"
         >
           {album.name}
         </div>
-        <h3
-          data-tippy-content={Spicetify.Locale.formatDate(
-            new Date(album.release_date),
-            {
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-            },
-          )}
-        >
-          {new Date(album.release_date).getFullYear()}
-        </h3>
+        <div className="line-clamp-1 inline-block text-sm text-spice-subtext">
+          <time
+            className="inline"
+            data-tippy-content={Spicetify.Locale.formatDate(
+              new Date(album.release_date),
+              {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              },
+            )}
+            dateTime={album.release_date}
+          >
+            {new Date(album.release_date).getFullYear()}
+          </time>
+          {" • "}
+          {album.artists.map((artist, index) => (
+            <React.Fragment key={artist.name}>
+              <a className="inline !text-spice-subtext" href={artist.uri}>
+                {artist.name}
+              </a>
+              {index !== album.artists.length - 1 && ", "}
+            </React.Fragment>
+          ))}
+        </div>
       </div>
       <div className="absolute right-0 top-0 z-0 flex size-full scale-90 rounded-md transition-all duration-300 ease-ease group-hover:scale-100 group-hover:bg-spice-main-elevated"></div>
     </a>
